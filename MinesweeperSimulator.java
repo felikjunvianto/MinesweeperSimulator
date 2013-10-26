0d91f0aac404a4773d8457e4c7d55d49649131c8
Felik Junvianto <felikjunvianto@yahoo.co.id>
2013-10-26 21:30:44 +0700

    edit label + tambah Java.gitignore

--- MinesweeperSimulator.java
+++ MinesweeperSimulator.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -7,17 +9,49 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class MinesweeperSimulator extends JFrame{
-	public MinesweeperSimulator(){
 
+	private static JTextField field[];
+	private static JLabel label[];
+
+	public MinesweeperSimulator() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setSize(700, 500);
+		setTitle("Minesweeper Simulator");
+		setLayout(new BorderLayout());
+	}
+
+	private static JPanel makeMenu() {
+		JPanel ret = new JPanel();
+		ret.setLayout(new BorderLayout());
+		
+		JPanel textFieldMenu = new JPanel();
+		textFieldMenu.setLayout(new GridLayout(5, 2));
+		field = new JTextField[5];
+		label = new JLabel[5];
+		String labelName[] = {"Width", "Height", "Mines", "Seed", "Interval (ms)"};
+
+		for(int i = 0; i < 5; i++) {
+			field[i] = new JTextField();
+			label[i] = new JLabel(labelName[i]);
+
+			textFieldMenu.add(label[i]);
+			textFieldMenu.add(field[i]);
+		}
+
+		ret.add(textFieldMenu, BorderLayout.CENTER);
+		ret.add(new JButton("Start"), BorderLayout.SOUTH);
+
+		return ret;
 	}
 
 	public static void main(String args[]){
 		MinesweeperSimulator frame = new MinesweeperSimulator();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setSize(700, 500);
 		
-		Field a = new Field();
-		frame.add(a);
+		Field grid = new Field();
+		JPanel menu = makeMenu();
+		frame.add(grid, BorderLayout.CENTER); grid.updateUI();
+		frame.add(menu, BorderLayout.EAST); menu.updateUI();
+
 	}
 }
